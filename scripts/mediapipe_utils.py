9acc82cc36157f134e7779604fbfbfb925406281
Mercuone <101574457+Shira0ri145@users.noreply.github.com>
2025-06-21 02:26:10 +0700

    Add files via upload

---
+++ scripts/mediapipe_utils.py
@@ -0,0 +1,52 @@
+# scripts/mediapipe_utils.py
+import cv2
+import numpy as np
+import mediapipe as mp
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+mp_face_mesh = mp.solutions.face_mesh
+
+def get_holistic_model():
+    return mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+def draw_styled_landmarks(image, results):
+    if results.face_landmarks:
+        mp_drawing.draw_landmarks(
+            image, results.face_landmarks, mp_face_mesh.FACEMESH_CONTOURS,
+            mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+            mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+        )
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(
+            image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+            mp_drawing.DrawingSpec(color=(80,22,10), thickness=1, circle_radius=1),
+            mp_drawing.DrawingSpec(color=(80,44,121), thickness=1, circle_radius=1)
+        )
+    if results.left_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+            mp_drawing.DrawingSpec(color=(121,22,76), thickness=1, circle_radius=1),
+            mp_drawing.DrawingSpec(color=(121,44,250), thickness=1, circle_radius=1)
+        )
+    if results.right_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+            mp_drawing.DrawingSpec(color=(245,117,66), thickness=1, circle_radius=1),
+            mp_drawing.DrawingSpec(color=(245,66,230), thickness=1, circle_radius=1)
+        )
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]) if results.pose_landmarks else np.zeros((33,4))
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]) if results.face_landmarks else np.zeros((468,3))
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]) if results.left_hand_landmarks else np.zeros((21,3))
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]) if results.right_hand_landmarks else np.zeros((21,3))
+    return np.concatenate([pose.flatten(), face.flatten(), lh.flatten(), rh.flatten()])
